a9734017aa987a6521e7fe332b6b1bce386b481a
Hassan Nawaz <hkbusnies786@gmail.com>
2025-12-26 06:29:27 +0500

    Update docusaurus.config.ts

--- robotics-book/docusaurus.config.ts
+++ robotics-book/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://robotics-book-hassannawaz02.vercel.app',
+  url: 'https://robotics-book-hk.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
